b966a016b35e7007c922c56a27bb894c6023c342
embedclub <scc.ddcx@gmail.com>
2016-12-27 15:53:24 +0800

    delete some c file

--- GPA2311.h
+++
@@ -1,7 +0,0 @@
-#include <reg52.h>
-
-sbit pga_cs					=P1^1;
-sbit pga_di					=P1^2;
-sbit pga_sk					=P1^3;
-
-extern pga_send(unsigned char buff_left,unsigned char buff_right);
--- while.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash 
-while :
-do
-    echo 0xB7 >/dev/null 
-done
-
-
