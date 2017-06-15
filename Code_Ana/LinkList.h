8f10cceedadb50b65b7aaf5cd61b5d284e3807ac
embedclub <embedclub@users.noreply.github.com>
2017-06-15 20:41:17 +0800

    Create LinkList.h

---
+++ Code_Ana/LinkList.h
@@ -0,0 +1,25 @@
+#ifndef _LINKLIST_H_
+#define _LINKLIST_H_
+
+typedef void LinkList;
+typedef struct _tag_LinkListNode LinkListNode;
+struct _tag_LinkListNode
+{
+    LinkListNode* next;
+};
+
+LinkList* LinkList_Create();
+
+void LinkList_Destroy(LinkList* list);
+
+void LinkList_Clear(LinkList* list);
+
+int LinkList_Length(LinkList* list);
+
+int LinkList_Insert(LinkList* list, LinkListNode* node, int pos);
+
+LinkListNode* LinkList_Get(LinkList* list, int pos);
+
+LinkListNode* LinkList_Delete(LinkList* list, int pos);
+
+#endif
